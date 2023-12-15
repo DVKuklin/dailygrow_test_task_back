41cce6de51cbd6cc33e13bd5e8000c3986ce71a5
Kuklin Denis <gdvkuklin@gmail.com>
2023-12-15 15:42:17 +0300

    make nullable b24link field in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('b24_link');
+            $table->string('b24_link')->nullable();
             $table->timestamps();
         });
     }
