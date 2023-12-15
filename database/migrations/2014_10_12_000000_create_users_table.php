a31759d3e2a984acebecfe676925575c0fe8a026
Kuklin Denis <gdvkuklin@gmail.com>
2023-12-15 12:36:17 +0300

    add b24 link to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('b24_link');
             $table->timestamps();
         });
     }
