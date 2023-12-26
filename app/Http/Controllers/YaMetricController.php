0e1141fe2052d9c68b2b29ed04d277cb58b27b1b
Kuklin Denis <gdvkuklin@gmail.com>
2023-12-26 17:21:26 +0300

    get yandex Oauth token

---
+++ app/Http/Controllers/YaMetricController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Http;
+
+use Illuminate\Http\Request;
+
+class YaMetricController extends Controller
+{
+    public function getToken(Request $request) {
+        if (isset($request->code)) {
+            $res = Http::asForm()->post('https://oauth.yandex.ru/token',
+                [
+                    "grant_type"=>'authorization_code',
+                    "code" => $request->code,
+                    "client_id" => env('YA_METRIC_ClientID'),
+                    'client_secret' => env('YA_METRIC_ClientSecret')
+                ]
+            );
+
+            if (isset($res['access_token'])) {
+                $token = $res['access_token'];
+                $expires_in = $res['expires_in'];
+                $refresh_token = $res['refresh_token'];
+                $user = $request->user();
+                $user->makeVisible('ya_metric_token');
+                $user->ya_metric_token = $token;
+                $user->save();
+                return response()->json(['status'=>'success'],200);
+            }
+            return response()->json(['status'=>'fail'],200);
+        }
+        return response()->json(['status'=>'fail'],200);
+    }
+
+    public function checkConnection(Request $request) {
+        $user = $request->user();
+        $user->makeVisible('ya_metric_token');
+        if ($user->ya_metric_token == null) {
+            return response()->json(['status'=>'notConnection','messge'=>'Соединение не настроено']);
+        }
+        $res = Http::withToken($user->ya_metric_token)->get('https://api-metrika.yandex.net/management/v1/counters');
+        if (isset($res['errors'])) {
+            return response()->json(['status'=>'notConnection'],200);
+        }
+        return response()->json(['status'=>'success'],200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\{AuthController, B24Controller};
+use App\Http\Controllers\{AuthController, B24Controller, YaMetricController};
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +22,11 @@ Route::controller(AuthController::class)->group(function(){
 
 Route::controller(B24Controller::class)->middleware('auth:sanctum')->prefix('b24')->group(function(){
     Route::get('/get-analytics','getAnalytics');
+});
+
+Route::controller(YaMetricController::class)->prefix('ya-metric')
+                                        ->middleware('auth:sanctum')
+                                        ->group(function(){
+    Route::post('/get-token','getToken');
+    Route::get('/check-connection','checkConnection');
 });
