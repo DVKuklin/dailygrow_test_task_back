505dfb4dd3e1bab5e07590aa4bc4a80b6fe120f7
Kuklin Denis <gdvkuklin@gmail.com>
2024-02-20 09:18:38 +0300

    ym-test

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{AuthController, B24Controller, YaDirectController, YaMetricController};
 
+use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -43,4 +46,38 @@ Route::controller(YaDirectController::class)->prefix('ya-direct')
                                         ->group(function(){
     Route::post('/get-token','getToken');
     Route::post('/get-campaigns','getCampaigns');
+});
+
+
+
+Route::post('/ym-test', function (Request $request) {
+    $baseUrlStatic = 'https://api-metrika.yandex.net/stat/v1/data';
+    $token = 'y0_AgAAAAAiqK2-AAsxSAAAAAD5X6XUAABKkqTKy0xDgoEvYByF9To69hlhDA';
+    $counter = '1226165';
+
+    //Kuklin
+    // $baseUrlStatic = 'https://api-metrika.yandex.net/stat/v1/data';
+    // $token = 'y0_AgAAAAAQJVw7AAsxIgAAAAD5PZBlAAAcd4wJtNlN3pjFpgTe1mSWZRV7Iw';
+    // $counter = '96302384';
+
+
+
+    //DirectPlatformType - тип рекламной площадки
+    //DirectPlatformTypeName - 
+    //DirectPlatform - рекламная площадка
+    //DirectSearchPhrase - поисковая фраза
+
+    $res = Http::withToken($token)->get($baseUrlStatic,[
+        'metrics'=>'ym:s:visits',
+        // 'dimensions'=>'ym:s:lastSignDirectClickOrderName',
+        // 'dimensions'=>'ym:s:lastSignDirectClickOrder',
+        'dimensions'=>'ym:s:lastSignDirectClickOrder,ym:s:lastSignDirectSearchPhrase',
+        // 'dimensions'=>'ym:s:lastSignDirectBannerGroup',DirectClickOrderName
+        'ids'=>[$counter],
+        'date1'=>Carbon::parse('12.01.2024')->isoFormat('YYYY-MM-DD'),
+        'date2'=>Carbon::parse('17.02.2024')->isoFormat('YYYY-MM-DD'),
+        'lang'=>'ru',
+    ])->json();
+
+    return $res;
 });
